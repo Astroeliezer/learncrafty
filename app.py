d651200653fc8073de9e73ac5ced119a58e4f656
Astroeliezer <132561273+Astroeliezer@users.noreply.github.com>
2023-10-13 20:33:03 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,61 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, session
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__)
+app = Flask(__name, template_folder="templates")
+app.config['UPLOAD_FOLDER'] = 'uploads'  # Define the UPLOAD_FOLDER here
+
+# Initialize data as an empty DataFrame
+data = pd.DataFrame()
 
 # Sample data storage (you can replace this with a database)
 data = None
+
+def process_data(data):
+    if not data.empty:
+        try:
+            # Clean the data (e.g., handle missing values)
+            data = data.dropna()
+
+            # Process the data (e.g., calculate new variables)
+            data['new_variable'] = data['column1'] * data['column2']
+
+            # You can perform additional data processing here
+
+            # Return the processed data as a list of dictionaries
+            processed_data = data.to_dict(orient='records')
+            return processed_data
+        except Exception as e:
+            return {'error': str(e)}
+    else:
+        return {'error': 'No data to process.'}
+
+# Data analysis function
+def analyze_data(data):
+    if not data.empty:
+        # Perform data analysis (you can customize this based on your data)
+        # For example, clustering using K-Means
+        X = data[['feature1', 'feature2']]  # Select the features for analysis
+        scaler = StandardScaler()
+        X_scaled = scaler.fit_transform(X)
+        kmeans = KMeans(n_clusters=3)
+        data['cluster'] = kmeans.fit_predict(X_scaled)
+
+        # Data Visualization
+        # Example: Create a bar chart
+        bar_data = data.groupby('cluster')['feature1'].count()
+        bar_data.plot(kind='bar')
+        plt.title('Data Analysis Results')
+        plt.xlabel('Cluster')
+        plt.ylabel('Count')
+        plt.savefig('analysis_result.png')  # Save the plot as an image
+
+# Flask routes
+@app.route('/')
+def hello():
+    return render_template('index.html')
 
 @app.route('/import-data', methods=['POST'])
 def import_data():
@@ -17,19 +68,22 @@ def import_data():
     if file:
         try:
             data = pd.read_csv(file)
+            session['data'] = data  # Store data in the session for future use
             return jsonify({'message': 'Data imported successfully'}), 200
         except Exception as e:
             return jsonify({'error': str(e)}), 400
 
 @app.route('/process-data', methods=['GET'])
-def process_data():
-    global data
-    if data is None:
-        return jsonify({'error': 'No data imported yet'}), 400
+def process_data_route():
+    data = session.get('data', None)  # Retrieve data from the session
+    processed_data = process_data(data)
+    return jsonify(processed_data)
 
-    # Add data processing code here
+@app.route('/analyze-data', methods=['GET'])
+def analyze_data_route():
+    data = session.get('data', None)  # Retrieve data from the session
+    analyze_data(data)  # Call the data analysis function
+    return jsonify({'message': 'Data analysis and visualization complete'})
 
-    return jsonify({'message': 'Data processed successfully'}), 200
-
-if __name__ == '__main__':
+if __name__ == '__main':
     app.run(debug=True)
--- script.js
+++ script.js
@@ -13,40 +13,104 @@ document.getElementById("file-input").addEventListener("change", function (event
         document.getElementById("data-visualization").style.display = "block";
     }
 });
+document.getElementById("analyze-button").addEventListener("click", function () {
+    fetch('/analyze-data')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        // Display the results, e.g., show the generated image in the frontend
+        const resultImage = document.createElement('img');
+        resultImage.src = 'analysis_result.png';
+        document.getElementById('results-container').appendChild(resultImage);
+    });
+});
 
 // Data Visualization
 document.getElementById("visualization-type").addEventListener("change", function () {
     const selectedType = this.value;
-    const data = {
-        // Your data for visualization
-    };
-
-    const canvas = document.getElementById("visualization-canvas");
-    const ctx = canvas.getContext("2d");
-
-    // Customize the visualization based on the selected type
     if (selectedType === "bar") {
-        // Example: Create a colorful bar chart using Chart.js
-        new Chart(ctx, {
-            type: "bar",
-            data: {
-                labels: data.labels,
+        // Create a bar chart
+        // Add your code here to create a bar chart
+        if (selectedType === "bar") {
+            const data = {
+                labels: ['Category A', 'Category B', 'Category C'],
                 datasets: [{
-                    label: "Values",
-                    data: data.values,
-                    backgroundColor: ["#FF5733", "#33FF8E", "#3373FF", "#E433FF"],
+                    label: 'Values',
+                    data: [10, 20, 30],
+                    backgroundColor: ['red', 'green', 'blue'],
                 }],
-            },
-        });
+            };
+        
+            const options = {
+                responsive: true,
+            };
+        
+            // Create a bar chart
+            new Chart(document.getElementById("visualization-canvas"), {
+                type: "bar",
+                data: data,
+                options: options,
+            });
+        }
+        
     } else if (selectedType === "line") {
-        // Example: Create a colorful line chart
-        // Add your code here
+        // Create a line chart
+        // Add your code here to create a line chart
+        if (selectedType === "line") {
+            const data = {
+                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+                datasets: [{
+                    label: 'Values',
+                    data: [10, 15, 20, 18, 25],
+                    borderColor: 'blue',
+                    fill: false,
+                }],
+            };
+        
+            const options = {
+                responsive: true,
+            };
+        
+            // Create a line chart
+            new Chart(document.getElementById("visualization-canvas"), {
+                type: "line",
+                data: data,
+                options: options,
+            });
+        }
+        
     } else if (selectedType === "pie") {
-        // Example: Create a colorful pie chart
-        // Add your code here
+        // Create a pie chart
+        // Add your code here to create a pie chart
+        if (selectedType === "pie") {
+            const data = {
+                labels: ['Category A', 'Category B', 'Category C'],
+                datasets: [{
+                    data: [30, 40, 50],
+                    backgroundColor: ['red', 'green', 'blue'],
+                }],
+            };
+        
+            const options = {
+                responsive: true,
+            };
+        
+            // Create a pie chart
+            new Chart(document.getElementById("visualization-canvas"), {
+                type: "pie",
+                data: data,
+                options: options,
+            });
+        }
+        
     } else if (selectedType === "heatmap") {
-        // Example: Create a colorful heatmap
-        // Add your code here
+        // Create a heat map
+        // Add your code here to create a heat map
+        if (selectedType === "heatmap") {
+            // Create a heatmap with D3.js (customize based on your data)
+            // Example: https://www.d3-graph-gallery.com/graph/heatmap_basic.html
+        }
+        
     }
 });
 
